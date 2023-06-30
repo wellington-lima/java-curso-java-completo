395e17463e330ac0ed980781d2b01b269140eb81
Wellington Santos <wellington.santos-g4f@abdi.com.br>
2023-06-30 15:23:40 -0300

    criado endpoint userResource findById

--- cursospringboot/src/main/java/com/sophysistemas/cursospringboot/resources/UserResource.java
+++ cursospringboot/src/main/java/com/sophysistemas/cursospringboot/resources/UserResource.java
@@ -1,19 +1,33 @@
 package com.sophysistemas.cursospringboot.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sophysistemas.cursospringboot.entities.User;
+import com.sophysistemas.cursospringboot.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
-
+	
+	@Autowired
+	private UserService service;
+	
 	@GetMapping
-	public ResponseEntity<User> findAll() {
-		User user = new User(1L, "Wellington", "wsantos@gmail.com", "6199998888", "123456");
+	public ResponseEntity<List<User>> findAll() {
+		List<User> users = service.findAll(); 
+		return ResponseEntity.ok().body(users);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id) {
+		User user = service.findById(id); 
 		return ResponseEntity.ok().body(user);
 	}
 }
